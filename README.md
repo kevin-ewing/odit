9651ed9bace9b2b6f6e17071acd134849bbf996c
Kevin Ewing <kewing@middlebury.edu>
2022-10-20 14:59:00 -0400

    GPT-3 working on git commit, more finetuning in prompt to be made but overall good

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Open Dialogue for Git (CS 701 Senior Seminar Project)
 
 ### Prerequisites
 Python 3.8
+OpenAI GPT-3 access and api key.
 
 ### Virtual Environment for openAI testing
 ```{bash}
@@ -12,7 +13,10 @@ cd openAI
 python3.8 -m venv venv
 source venv/bin/activate
 pip3 install -r openAI_requirements.txt
+export OPENAI_API_KEY=''
 ```
+
+Also export 
 
 ### Virtual Environment for dialogue testing
 ```{bash}
--- openAI/openAI_test.py
+++ openAI/openAI_test.py
@@ -1,46 +1,40 @@
 import os
 import openai
-import json
-import git
-import re
+import pygit2
 
-# Testing the openAI gpt-3 code-davinci-002 code translator
+OPEN_AI_PROMPT = "Summarize, in a few sentences, what has changed in the git diff\n\n"
+
 # $ export OPENAI_API_KEY='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'
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
- 
-# def get_diff_added():
-#   repo = git.Repo('.')
-#   t = repo.head.commit.tree
-#   added_diff = ''
-#   removed_diff = ''
-#   for line in str.splitlines(repo.git.diff(t)):
-#     if re.match('^[a-zA-Z]+', line) is not None:
-#       added_diff = added_diff + "\n" + line
-  
-#   print(added_diff)
 
-def main():
-  response = None
-  # diff = get_diff_added()
+def open_ai_call(diff):
+  prompt = OPEN_AI_PROMPT + diff
+  response = openai.Completion.create(
+    model="text-davinci-002",
+    prompt=prompt,
+    temperature=0.7,
+    max_tokens=100,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+  return response
 
-  try:
-    response = openai.Completion.create(
-      model="code-davinci-002",
-      prompt="class Log:\n    def __init__(self, path):\n        dirname = os.path.dirname(path)\n        os.makedirs(dirname, exist_ok=True)\n        f = open(path, \"a+\")\n\n        # Check that the file is newline-terminated\n        size = os.path.getsize(path)\n        if size > 0:\n            f.seek(size - 1)\n            end = f.read(1)\n            if end != \"\\n\":\n                f.write(\"\\n\")\n        self.f = f\n        self.path = path\n\n    def log(self, event):\n        event[\"_event_id\"] = str(uuid.uuid4())\n        json.dump(event, self.f)\n        self.f.write(\"\\n\")\n\n    def state(self):\n        state = {\"complete\": set(), \"last\": None}\n        for line in open(self.path):\n            event = json.loads(line)\n            if event[\"type\"] == \"submit\" and event[\"success\"]:\n                state[\"complete\"].add(event[\"id\"])\n                state[\"last\"] = event\n        return state\n\n\"\"\"\nHere's what the above class is doing:\n1.",
-      temperature=0,
-      max_tokens=64,
-      top_p=1.0,
-      frequency_penalty=0.0,
-      presence_penalty=0.0,
-      stop=["\"\"\""]
-    )
-  except openai.error.InvalidRequestError as e:
-    print(e)
 
-  if response is not None:
-    text_response = response.choices[0]["text"]
-    print(text_response)
+def get_git_repo():
+    curr_dir = os.getcwd()
+    repo = pygit2.Repository(curr_dir)
+    return repo 
+
+
+def get_diff():
+    repo  = get_git_repo()
+    diff = repo.diff()
+    return diff.patch
+
 
 if __name__ == "__main__":
-    main()
+    diff = get_diff()
+    response = open_ai_call(diff)
+    print(response.choices[0].text)
